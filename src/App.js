068bc3cf90809ad9293024586c517beb04731e3c
Artur <helter@wp.pl>
2022-03-24 14:33:25 +0100

    implement useSelector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import './App.css';
+import { useSelector } from 'react-redux';
+
 function App() {
-  return <div className="App">Starting</div>;
+  const toDoList = useSelector((state) => state.toDoList);
+
+  let showList = toDoList.map((el) => <li key={el}>{el}</li>);
+  return (
+    <div className="App">
+      <ul>{showList.length === 0 ? 'Please enter your activity' : showList}</ul>
+    </div>
+  );
 }
 
 export default App;
