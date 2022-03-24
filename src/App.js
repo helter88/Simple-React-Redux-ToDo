5ebb0b39852785c2a8e9c6b345ef73c84b1f330a
Artur <helter@wp.pl>
2022-03-24 16:04:55 +0100

    add input and button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function App() {
   const toDoList = useSelector((state) => state.toDoList);
   const dispatch = useDispatch();
+  const [activity, setActivity] = useState('');
 
   const deleteHandler = (el) => {
     dispatch({ type: 'REMOVE', payload: el });
+  };
+
+  const activityHandler = (e) => {
+    console.log(e.target.value);
   };
 
   let showList = toDoList.map((el) => (
@@ -16,6 +22,13 @@ function App() {
   ));
   return (
     <div className="App">
+      <h1>Your List</h1>
+      <input
+        type="text"
+        placeholder="You can write your activity here:"
+        onChange={activityHandler}
+      />
+      <button type="button">Add to list</button>
       <ul>{showList.length === 0 ? 'Please enter your activity' : showList}</ul>
     </div>
   );
